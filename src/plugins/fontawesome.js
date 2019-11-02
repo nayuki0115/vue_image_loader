6527295b7ddfe0f91a5eb6b3003411b0f2060391
Annie Wu <annie25506@gmail.com>
2019-11-02 23:37:12 +0800

    feat: vue-fontawsome import

---
+++ src/plugins/fontawesome.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { dom } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(fas, far, fab)
+dom.watch();
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
