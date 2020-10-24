7b9122c76e00f64f112e796686c38746393a35da
Annie Wu <annie25506@gmail.com>
2020-10-25 00:38:36 +0800

    fix: imageloader for style

--- src/components/ImageLoader/ImageLoader.vue
+++ src/components/ImageLoader/ImageLoader.vue
@@ -1,104 +1,60 @@
 <template>
-  <div class="image-wrapper" v-if="currentShow">
-    <div class="image-content">
-      <div class="image-title">{{title}}</div>
-      <div class="image-button image-close" @click="handle_close">
-        <!-- <i class="fas fa-times"></i> -->
-      </div>
+  <div class="image-wrapper" v-if="syncShow">
+    <i class="fas fa-search-plus"></i>
+    <div class="imgage-content">
+      <div class="image-title" v-if="images[imageIndex]['imgTitle']  && images[imageIndex]['imgTitle'] !== ''">{{images[imageIndex]['imgTitle'] }}</div>
+      <div class="image-button image-close" @click="handleClose"></div>
       <div>
-        <span v-if="left_button === true" class="arrow-left" @click="handle_arrow_button('left')"><i class="el-icon-caret-left"></i></span>
-        <div class="img-box"><img :src="images" alt="" :style="imageStyle"></div>
-        <span v-if="right_button ===  true" class="arrow-right" @click="handle_arrow_button('right')"><i class="el-icon-caret-right"></i></span>
-      </div>
-      <div class="image-tool">
-        <span class="image-tool-button" @click="handle_tool_button('zoom-in')"><i class="fas fa-search-plus"></i></span>
-        <span class="image-tool-button" @click="handle_tool_button('zoom-out')"><i class="fas fa-search-minus"></i></span>
-        <span class="image-tool-button" @click="handle_tool_button('rotate-left')"><i class="fas fa-undo-alt"></i></span>
-        <span class="image-tool-button" @click="handle_tool_button('rotate-right')"><i class="fas fa-redo-alt"></i></span>
+        <span></span>
+        <div class="img-box" v-if="images[imageIndex]['imgUrl']  && images[imageIndex]['imgUrl'] !== ''"><img :src="images[imageIndex]['imgUrl']" alt="" ></div>
+        <span></span>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-
 export default {
-  props: ['show', 'images', 'title', 'right_button', 'left_button'],
+  props: {
+    'show': {
+      type: Boolean,
+      required: true
+    },
+    'images': {
+      type: Array,
+      required: true
+    }
+  },
   data () {
     return {
-      currentShow: false,
-      imageStyle: {
-        'width': '',
-        'transform': ''
-      },
-      imageWidth: '',
-      imageRotate: 0
+      imageIndex: 0
     }
   },
   created () {
 
   },
-  watch: {
-    show: {
-      handler (val) {
-        this.currentShow = val
+  computed: {
+    syncShow: {
+      get () {
+        return this.show
       },
-      immediate: true
-    },
-    images: {
-      handler (val) {
-        let img = new Image()
-        img.src = this.images
-        img.onload = async () => {
-          this.imageWidth = img.width
-          this.$set(this.imageStyle, 'width', `${img.width}px`)
-
-          this.imageRotate = 0
-          this.$set(this.imageStyle, 'transform', `rotate(0deg)`)
-        }
-      },
-      immediate: true,
-      deep: true
+      set (value) {
+        console.log('value', value)
+        this.$emit('update:show', value)
+        // emit
+      }
     }
   },
   methods: {
-    handle_close () {
+    handleClose () {
       console.log('close')
-      this.currentShow = false
-      this.$set(this, 'imageStyle', {})
-      this.$emit('update:show', this.currentShow)
-    },
-    handle_arrow_button (action) {
-      this.$emit('click_button', action)
-    },
-    handle_tool_button (featuresName) {
-      console.log(featuresName)
-
-      switch (featuresName) {
-        case 'zoom-in':
-          this.$set(this, 'imageWidth', this.imageWidth + 50)
-          this.$set(this.imageStyle, 'width', `${this.imageWidth + 50}px`)
-          break
-        case 'zoom-out':
-          this.$set(this, 'imageWidth', this.imageWidth - 50)
-          this.$set(this.imageStyle, 'width', `${this.imageWidth - 50}px`)
-          break
-        case 'rotate-left':
-          this.$set(this, 'imageRotate', this.imageRotate - 5)
-          this.$set(this.imageStyle, 'transform', `rotate(${this.imageRotate - 5}deg)`)
-          break
-        case 'rotate-right':
-          this.$set(this, 'imageRotate', this.imageRotate + 5)
-          this.$set(this.imageStyle, 'transform', `rotate(${this.imageRotate + 5}deg)`)
-          break
-      }
+      this.syncShow = false
     }
   }
-
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
   .image-wrapper {
     z-index: 2009;
     position: fixed;
@@ -117,96 +73,6 @@ export default {
     box-sizing: border-box;
   }
 
-  .image-button {
-    background-color: rgba(0, 0, 0, 0.5);
-    border-radius: 50%;
-    cursor: pointer;
-    height: 80px;
-    overflow: hidden;
-    position: absolute;
-    right: -40px;
-    top: -40px;
-    -webkit-transition: background-color 0.15s;
-    transition: background-color 0.15s;
-    width: 80px;
-    z-index: 1;
-  }
-
-  .image-button::before {
-    bottom: 15px;
-    left: 15px;
-    position: absolute;
-  }
-
-  .image-close::before {
-    background-position: -260px 0;
-    content: 'X';
-    color: #FFF
-  }
-
-  .arrow-left{
-    top: 50%;
-    transform: translateY(-50%);
-    width: 44px;
-    height: 44px;
-    font-size: 24px;
-    color: #fff;
-    background-color: #606266;
-    border-color: #fff;
-    left: 40px;
-    position: absolute;
-    z-index: 1;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border-radius: 50%;
-    opacity: .8;
-    cursor: pointer;
-    box-sizing: border-box;
-    user-select: none;
-  }
-
-  .arrow-right{
-    right: 40px;
-    text-indent: 2px;
-    top: 50%;
-    transform: translateY(-50%);
-    width: 44px;
-    height: 44px;
-    font-size: 24px;
-    color: #fff;
-    background-color: #606266;
-    border-color: #fff;
-    position: absolute;
-    z-index: 1;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border-radius: 50%;
-    opacity: .8;
-    cursor: pointer;
-    box-sizing: border-box;
-    user-select: none;
-  }
-
-  .img-box {
-    text-align: center;
-    height: 95vh;
-    overflow: auto;
-    padding: 50px 0;
-    box-sizing: border-box;
-    &:before {
-      content:'';
-      display: inline-block;
-      vertical-align: middle;
-      width: 0;
-      height: 100%;
-    }
-    img {
-      // max-width: 100%;
-      vertical-align: middle;
-    }
-  }
   .image-title{
     width: 600px;
     background-color: rgba(0, 0, 0, 0.5);
@@ -238,74 +104,54 @@ export default {
       width: 200px;
       left: calc(50% - 100px);
     }
-
   }
 
-  .image-tool{
-    width: 400px;
-    left: calc(50% - 200px);
-    color: white;
-    z-index: 1;
-    position: absolute;
-    height: 5vh;
-    text-align: center;
+  .image-button {
     background-color: rgba(0, 0, 0, 0.5);
-    border-radius: 6px;
+    border-radius: 50%;
+    cursor: pointer;
+    height: 80px;
+    overflow: hidden;
+    position: absolute;
+    right: -40px;
+    top: -40px;
+    -webkit-transition: background-color 0.15s;
+    transition: background-color 0.15s;
+    width: 80px;
+    z-index: 1;
+  }
 
-    @media screen and (max-width: 1280px) {
-      width: 350px;
-      left: calc(50% - 175px);
-    }
+  .image-button::before {
+    bottom: 15px;
+    left: 15px;
+    position: absolute;
+  }
 
-    @media screen and (max-width: 960px) {
-      width: 300px;
-      left: calc(50% - 150px);
-    }
+  .image-close::before {
+    // font-family: "Font Awesome 5 Free";
+    // font-weight: 900;
+    // content: '\f00d';
+    background-position: -260px 0;
+    content: '✖';
+    color: #FFF;
+  }
 
-    @media screen and (max-width: 540px) {
-      width: 250px;
-      left: calc(50% - 175px);
-    }
-
-    @media screen and (max-width: 320px) {
-      width: 200px;
-      left: calc(50% - 100px);
-    }
-
-    .image-tool-button{
-      font-size: 35px;
-      color: #fff;
-      border-color: #fff;
-      padding: 0 15px;
-      cursor: pointer;
+  .img-box {
+    text-align: center;
+    height: 95vh;
+    overflow: auto;
+    padding: 50px 0;
+    box-sizing: border-box;
+    &:before {
+      content:'';
       display: inline-block;
       vertical-align: middle;
-
-      @media screen and (max-width: 1280px) {
-        font-size: 30px;
-      }
-
-      @media screen and (max-width: 960px) {
-        font-size: 25px;
-      }
-
-      @media screen and (max-width: 540px) {
-        font-size: 20px;
-      }
-
-      @media screen and (max-width: 320px) {
-        font-size: 15px;
-      }
+      width: 0;
+      height: 100%;
+    }
+    img {
+      // max-width: 100%;
+      vertical-align: middle;
     }
   }
-
-  .image-tool:before {
-    display: inline-block;
-    vertical-align: middle;
-    content: '';
-    height: 100%;
-    width: 0px;
-    background: red;
-  }
-
 </style>
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,18 +1,15 @@
 <template>
   <div>
-    <div>
-      單張打api形式：
-      <button @click="handle_simgle_picture()">點我看圖</button>
+    <div class="single-container">
+      單張形式：<br/>
+      <!-- <button @click="handleSimplePicture()">點我看圖</button> -->
+      <img class="single-images" src="https://images.pexels.com/photos/3114350/pexels-photo-3114350.jpeg" @click="handleSimplePicture()" alt="單張形式" />
       <ImageLoader
-        :show.sync="simgle_image_show"
+        :show.sync="singleImageShow"
         :images="images"
-        :title="image_title"
-        :right_button="image_right_button"
-        :left_button="image_left_button"
-        @click_button="handle_arrow_button"
       />
     </div>
-    <div>
+    <!-- <div>
       一次給多張圖片形式：
       <button @click="handle_multiple_picture()">點我看圖</button>
       <ImageLoader
@@ -23,7 +20,7 @@
         :left_button="image_left_button"
         @click_button="handle_arrow_button"
       />
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -38,129 +35,40 @@ export default {
   data () {
     return {
       images: null,
-      image_title: null,
-      simgle_image_show: false,
-      multiple_image_show: false,
-      image_current_digital_id: null,
-      image_current_level: null,
-      image_current_metadata_id: null,
-      image_prev_digital_id: null,
-      image_prev_level: null,
-      image_prev_metadata_id: null,
-      image_next_digital_id: null,
-      image_next_level: null,
-      image_next_metadata_id: null,
-      image_left_button: false,
-      image_right_button: false
+      singleImageShow: false
     }
   },
   created () {
     // temp
-    this.images = 'https://images.pexels.com/photos/3114350/pexels-photo-3114350.jpeg'
+    this.images = [
+      {
+        imgUrl: 'https://images.pexels.com/photos/3114350/pexels-photo-3114350.jpeg',
+        imgTitle: 'Hi'
+      }
+    ]
   },
   methods: {
     async init () {
 
     },
-    handle_simgle_picture () {
-      this.simgle_image_show = true
-    },
-    handle_multiple_picture () {
-      this.multiple_image_show = true
-    },
-    async handle_arrow_button (val) {
-      console.log(val)
-
-      // if(val === 'right'){
-      //   let preview_res = await api.digital_object_preview(
-      //     {
-      //       level: this.image_next_level,
-      //       meta_data_id: this.image_next_metadata_id,
-      //       digital_id: this.image_next_digital_id
-      //     }
-      //   )
-      //   if(preview_res['success'] === true &&  preview_res['payload']['curr'] != null){
-      //     this.image_current_digital_id = preview_res['payload']['curr']['id']
-      //     this.image_current_level = preview_res['payload']['curr']['levels_of_control']
-      //     this.image_current_metadata_id = preview_res['payload']['curr']['meta_data_id']
-      //     this.image_title = preview_res['payload']['curr']['image_name_public']
-
-      //     if(preview_res['payload']['prev'] != null){
-      //       this.image_prev_digital_id = preview_res['payload']['prev']['id']
-      //       this.image_prev_level = preview_res['payload']['prev']['levels_of_control']
-      //       this.image_prev_metadata_id = preview_res['payload']['prev']['meta_data_id']
-      //       this.image_left_button = true
-      //     }else if(preview_res['payload']['prev'] == null){
-      //       this.image_prev_digital_id = null
-      //       this.image_prev_level = null
-      //       this.image_prev_metadata_id = null
-      //       this.image_left_button = false
-      //     }
-
-      //     if(preview_res['payload']['next'] != null){
-      //       this.image_next_digital_id =  preview_res['payload']['next']['id']
-      //       this.image_next_level = preview_res['payload']['next']['levels_of_control']
-      //       this.image_next_metadata_id = preview_res['payload']['next']['meta_data_id']
-      //       this.image_right_button = true
-      //     }else if(preview_res['payload']['next'] == null) {
-      //       this.image_next_digital_id = null
-      //       this.image_next_level = null
-      //       this.image_next_metadata_id = null
-      //       this.image_right_button = false
-      //     }
-
-      //     let imgs = this.get_img_url(preview_res['payload']['curr']['url'])
-      //     this.images = imgs
-      //   }
-
-      // }else if (val === 'left'){
-      //   let preview_res = await api.digital_object_preview(
-      //     {
-      //       level: this.image_prev_level,
-      //       meta_data_id: this.image_prev_metadata_id,
-      //       digital_id: this.image_prev_digital_id
-      //     }
-      //   )
-
-      //   if(preview_res['success'] === true &&  preview_res['payload']['curr'] != null){
-      //     this.image_current_digital_id = preview_res['payload']['curr']['id']
-      //     this.image_current_level = preview_res['payload']['curr']['levels_of_control']
-      //     this.image_current_metadata_id = preview_res['payload']['curr']['meta_data_id']
-      //     this.image_title = preview_res['payload']['curr']['image_name_public']
-
-      //     if(preview_res['payload']['prev'] != null){
-      //       this.image_prev_digital_id = preview_res['payload']['prev']['id']
-      //       this.image_prev_level = preview_res['payload']['prev']['levels_of_control']
-      //       this.image_prev_metadata_id = preview_res['payload']['prev']['meta_data_id']
-      //       this.image_left_button = true
-      //     }else if(preview_res['payload']['prev'] == null){
-      //       this.image_prev_digital_id = null
-      //       this.image_prev_level = null
-      //       this.image_prev_metadata_id = null
-      //       this.image_left_button = false
-      //     }
-
-      //     if(preview_res['payload']['next'] != null){
-      //       this.image_next_digital_id =  preview_res['payload']['next']['id']
-      //       this.image_next_level = preview_res['payload']['next']['levels_of_control']
-      //       this.image_next_metadata_id = preview_res['payload']['next']['meta_data_id']
-      //       this.image_right_button = true
-      //     }else if(preview_res['payload']['next'] == null) {
-      //       this.image_next_digital_id = null
-      //       this.image_next_level = null
-      //       this.image_next_metadata_id = null
-      //       this.image_right_button = false
-      //     }
-
-      //     let imgs = this.get_img_url(preview_res['payload']['curr']['url'])
-      //     this.images = imgs
-      //   }
-      // }
+    handleSimplePicture () {
+      this.$set(this, 'singleImageShow', true)
     }
   }
 }
 </script>
 
-<style>
-
+<style lang="scss">
+  .single-container {
+    margin: 10px;
+    .single-images {
+      width: 20vw;
+      border-radius: 5px;
+      cursor: pointer;
+      transition: all 0.5s ease-in;
+      &:hover {
+        opacity: 0.6;
+      }
+    }
+  }
 </style>
